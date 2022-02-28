bef538c37bdf78961af9ea4aba16708fc1f90ae6
André Luiz Alves Costa <andrelac963@gmail.com>
2022-02-28 01:50:15 -0300

    Dev: autentificação

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,18 +1,28 @@
+import { useState } from 'react'
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
 
 import styles from './styles.module.scss'
 
 export function SignInButton() {
-  const isUserLoggedIn = true
-  return isUserLoggedIn ? (
+  const [session, setSession] = useState(false)
+
+  return session ? (
     <button type="button" className={styles.signInButton}>
       <FaGithub color="#04d361" />
       André Luiz
-      <FiX color="#737380" className={styles.closeIcon} />
+      <FiX
+        color="#737380"
+        className={styles.closeIcon}
+        onClick={() => setSession(false)}
+      />
     </button>
   ) : (
-    <button type="button" className={styles.signInButton}>
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => setSession(true)}
+    >
       <FaGithub color="#eba417" />
       Sign In with Github
     </button>
